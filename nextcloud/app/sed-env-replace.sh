17a8d7fe2344dd19ef5e8f1074798da39cc66054
Felix Fischer <fischerfelix06@gmail.com>
2022-10-15 22:36:41 +0200

    use bash for script

--- nextcloud/app/sed-env-replace.sh
+++ nextcloud/app/sed-env-replace.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 input_file=$1
 commands=('s/pm.max_children = 5/pm.max_children = ${PM.MAX_CHILDREN}/g w /dev/stdout'
 's/pm.start_servers = 2/pm.start_servers = ${PM.START_SERVERS}/g w /dev/stdout'
