608ed6663c9fb9e28aecde8df2ef195456cc7b33
Felix Fischer <fischerfelix06@gmail.com>
2022-10-15 22:31:48 +0200

    edit php-fpm.d/www.conf to accept env variables

---
+++ nextcloud/app/sed-env-replace.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+input_file=$1
+commands=('s/pm.max_children = 5/pm.max_children = ${PM.MAX_CHILDREN}/g w /dev/stdout'
+'s/pm.start_servers = 2/pm.start_servers = ${PM.START_SERVERS}/g w /dev/stdout'
+'s/pm.min_spare_servers = 1/pm.min_spare_servers = ${PM.MIN_SPARE_SERVERS}/g w /dev/stdout'
+'s/pm.max_spare_servers = 3/pm.max_spare_servers = ${PM.MAX_SPARE_SERVERS}/g w /dev/stdout')
+
+for i in "${commands[@]}"; do
+        if [[ $(sed -i "$i" $input_file) ]]; then
+                echo "$i"
+        else
+                echo "error"
+                exit 1
+        fi
+done
